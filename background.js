c0678fbf404e28b62275be5ffe955c79015bba89
Isaiah Odhner <isaiahodhner@gmail.com>
2020-11-07 09:51:36 -0500

    Get extension working in chrome

--- background.js
+++ background.js
@@ -8,7 +8,24 @@ if (typeof browser !== "undefined" && browser.pageAction) {
 } else {
 	chrome.pageAction.onClicked.addListener(() => {
 		chrome.tabs.executeScript({
-			file: "mutagen.js"
+			// file: "mutagen.js"
+			code: `
+var script = document.createElement('script');
+document.body.appendChild(script);
+script.src = chrome.runtime.getURL("mutagen.js");
+`
+		});
+	});
+	chrome.runtime.onInstalled.addListener(function () {
+		chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+			chrome.declarativeContent.onPageChanged.addRules([{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						css: ["textarea"]
+					})
+				],
+				actions: [new chrome.declarativeContent.ShowPageAction()]
+			}]);
 		});
 	});
 }
