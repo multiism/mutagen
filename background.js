407c2fc605ef192582ac66716205b5425386aded
Isaiah Odhner <isaiahodhner@gmail.com>
2020-11-12 21:52:13 -0500

    Inject the script the same way for different browsers

--- background.js
+++ background.js
@@ -1,20 +1,16 @@
+const code = `
+var script = document.createElement('script');
+document.body.appendChild(script);
+script.src = (typeof browser !== "undefined" ? browser : chrome).runtime.getURL("mutagen.js");
+`;
 
 if (typeof browser !== "undefined" && browser.pageAction) {
 	browser.pageAction.onClicked.addListener(() => {
-		browser.tabs.executeScript({
-			file: "mutagen.js"
-		});
+		browser.tabs.executeScript({ code });
 	});
 } else {
 	chrome.pageAction.onClicked.addListener(() => {
-		chrome.tabs.executeScript({
-			// file: "mutagen.js"
-			code: `
-var script = document.createElement('script');
-document.body.appendChild(script);
-script.src = chrome.runtime.getURL("mutagen.js");
-`
-		});
+		chrome.tabs.executeScript({ code });
 	});
 	chrome.runtime.onInstalled.addListener(function () {
 		chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
